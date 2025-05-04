9c3cd66f243d355bf0ed814b050d7c346da777ae
abeer-abubaker <abeerabubaker2001@gmail.com>
2025-05-04 09:28:26 +0300

    Completed Header section: Logo, Navigation menu, and Contact info

--- js/script.js
+++ js/script.js
@@ -2,7 +2,22 @@
 
 
 // Nav open close
-
+   const body=document.querySelector('body'),
+         navMenu=body.querySelector('.menu-content'),
+         navClosebtn=body.querySelector('.navClose-btn'),
+         navOpenbtn=body.querySelector('.navOpen-btn');
+      if(navMenu && navOpenbtn){
+        navOpenbtn.addEventListener("click", ()=>{
+            navMenu.classList.add('open')
+            body.style.overflowY="hidden"
+        })
+      }
+      if(navMenu && navClosebtn){
+        navClosebtn.addEventListener("click", ()=>{
+            navMenu.classList.remove('open')
+            body.style.overflowY="scroll"
+        })
+      }
 
 // Change header bg color
 
