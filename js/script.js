f7d2e9332753bd73f1f53ff5cae462ae2c17d723
abeer-abubaker <abeerabubaker2001@gmail.com>
2025-05-04 19:59:44 +0300

    Completed Home section & Swiper.js: Added slider with image background and navigation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 // Swiper js
-
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabcursor:true,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
 // Nav open close
    const body=document.querySelector('body'),
